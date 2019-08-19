93ed8de401d86c322302f79b536288b3f7f8307d
Trevor McCauley <mccauley@adobe.com>
2019-08-19 00:31:25 -0400

    Updates

---
+++ precache-manifest.85133603340511aa13b45a43525c3557.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485cff71cd5a756ed883650c2da4b0b1",
+    "url": "/swiftui-react/index.html"
+  },
+  {
+    "revision": "4bf65a6cb0b40702d553",
+    "url": "/swiftui-react/static/js/2.88a251f6.chunk.js"
+  },
+  {
+    "revision": "2fb5ae6814f2a7757e35",
+    "url": "/swiftui-react/static/js/main.136f5461.chunk.js"
+  },
+  {
+    "revision": "37b717184f62c71ba79b",
+    "url": "/swiftui-react/static/js/runtime~main.0692c7c7.js"
+  }
+]);
